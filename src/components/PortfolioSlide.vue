7dcdb1ce2b726d7814b67e80c2e50564a36affe4
Tug Chianese <amorecbellum@gmail.com>
2023-07-21 18:55:53 +0200

    Updated favicon and added to the portfolio section

--- src/components/PortfolioSlide.vue
+++ src/components/PortfolioSlide.vue
@@ -5,8 +5,8 @@ import ExpandoCard from './ExpandoCard.vue';
 const projects = reactive([
   new Expando(
     'Portfolio',
-    'Vue 3 (Quasar-CLI), HTML/CSS/JS, SCSS, [Design]',
-    'The page you are on now!',
+    'Quasar-CLI (Vue 3), HTML/CSS/JS, SCSS, {Design}',
+    'This is the page you are on right now! I made this on my own time because I really enjoy working with Vue 3 and because I wanted to challenge myself.',
     `    <dl>
       <dt style="text-transform: uppercase">Overview</dt>
       <dd>
@@ -101,8 +101,8 @@ const projects = reactive([
   ),
   new Expando(
     'Yellowstone Job Application',
-    'HTML/CSS/JS, Bootstrap, SCSS, [Design]',
-    'A school project for my Javascript 2 class. The purpose was to redesign a job application to be easier to use.',
+    'HTML/CSS/JS, Bootstrap, SCSS, {Design}',
+    'The purpose of this project was to redesign a job application and create a proof-of-concept website. I included this to demonstrate the thought I put into UX and UI. ',
     `    <dl>
       <dt style="text-transform: uppercase">Overview</dt>
       <dd>
@@ -211,14 +211,177 @@ const projects = reactive([
       },
     ]
   ),
+  new Expando(
+    'Wireframes',
+    'Balsamiq Wireframes, {Design}',
+    "I was given the task to completely redesign a client's website. These are mockups I completed after studying the database schema and communicating with the client for feedback",
+    null,
+    null,
+    null,
+    'WireframeDemo.jpg',
+    [
+      {
+        title: 'Wireframe',
+        icon: 'architecture',
+        href: 'https://balsamiq.cloud/si4kiz6/pjr87ro/rC802?f=N4IgUiBcCMA0IDkpxAYWfAMhkAhHAsjgFpQAM8A0lANqgBOUI0AbACxsgC%2BsDTAggFYAzADFuvEI0ghBglgCYJfGQBEAorjLKpTAJxtUqndOZlBSnitnq9%2FE0wDs0QZy4BdLkA%3D%3D',
+        target: '_blank',
+      },
+    ]
+  ),
+  new Expando(
+    'Fact App',
+    'Xamarin Forms',
+    'A small project I did for mobile development. This app displayed "true facts" about me with little pictures. Pictures and facts sourced from ChatGPT and Midjourney. This was mainly a "dive into the language" project.',
+    null,
+    'info',
+    'commentary',
+    'FactDemo.jpg',
+    [
+      {
+        title: 'Code',
+        icon: 'code',
+        href: 'https://github.com/LegendaryJay/FactApp',
+        target: '_blank',
+      },
+    ]
+  ),
+  new Expando(
+    'Javascript Timer Final Project',
+    'Vue 3 (with Quasar plugin),HTML/CSS/JS, Pinia, Firebase (nosql)',
+    "This was an app that was supposed to help a user manage their time in order to be more productive. I've included this because I am particularly proud of the design of the code itself rather than the actual website.",
+    `    <dl>
+      <dt style="text-transform: uppercase">Overview</dt>
+      <dd>
+        <dl>
+          <dt><strong>Objectives</strong></dt>
+          <dd>
+            This project was the final project for my javascript 2 class. The
+            aim was to an assignment aimed at assessing our proficiency in the
+            design of the website itself and the design of the code behind it.
+            It was suppose to act as a pomodoro timer for productivity.
+          </dd>
+        </dl>
+      </dd>
+      <dd>
+        <dl>
+          <dt><strong>Target Audience</strong></dt>
+          <dd>
+            I made this essentiall for people like myself who benefit from
+            productivity tools
+          </dd>
+        </dl>
+      </dd>
+    </dl>
+    <hr />
+    <dl>
+      <dt style="text-transform: uppercase">Noteable Features</dt>
+      <dd>
+        <dl>
+          <dt><strong>Database interactions have low coupling</strong></dt>
+          <dd>
+            I spent much of my time making sure that the components within this
+            project could essentially be used anywhere by separating all the
+            database functions to an object that can be replaced. This is fairly
+            similar to the repository design pattern.
+          </dd>
+        </dl>
+      </dd>
+      <dd>
+        <dl>
+          <dt><strong>Well designed wireframe</strong></dt>
+          <dd>
+            The website does not quite match the wireframe due to time
+            constraints and my learning Vue 3 for the first time, but I am
+            extremely proud of how the wireframe turned out. I plan on finishing
+            the concept and recreating a working copy.
+          </dd>
+        </dl>
+      </dd>
+    </dl>
+    <hr />
+    <dl>
+      <dt style="text-transform: uppercase">Challenges and Solutions</dt>
+      <dd>
+        <dl>
+          <dt><strong>Database Separation of Concerns</strong></dt>
+          <dd>
+            It felt really wrong of me to hardcode database stuff everywhere
+            throughout the program. If I changed one thing, I would have to make
+            tons of changes. What I ended up doing is something similar to the
+            repository pattern. Essentially I only allowed things to interface
+            with a section called
+            <a
+              href="https://github.com/LegendaryJay/JavascriptFinal/blob/master/src/model/UserRepository.js"
+            >
+              UserRepository
+            </a>
+            which did pretty much all the specialized work while only showing
+            general commands to whatever implimented it.
+          </dd>
+        </dl>
+        <dl>
+          <dt><strong>Firebase issues</strong></dt>
+          <dd>
+            I had some sort of compatability issues with firebase at the time. I
+            remember it being really hard to work with due to conflicting
+            documentation (because the version was brand new at the time) too.
+            What I ended up doing is downgrading the firebase version. Suddenly,
+            everything worked as intended.
+          </dd>
+        </dl>
+        <dl>
+          <dt><strong>Firebase Authentication</strong></dt>
+          <dd>
+            Much of my time went into learning how firebase worked. This was all
+            new technology for me at the time so learning just took a little
+            extra time. I am unsure why I struggled with this, but the answer
+            was simply to read through the documentation from start to finish
+            after downgrading the version. After that, progressing was extremely
+            easy.
+          </dd>
+        </dl>
+      </dd>
+    </dl>
+    `,
+    'info',
+    'commentary',
+    '',
+    [
+      {
+        title: 'Wireframe',
+        icon: 'architecture',
+        href: 'https://balsamiq.cloud/si4kiz6/p7vg6g1',
+        target: '_blank',
+      },
+      {
+        title: 'Code',
+        icon: 'code',
+        href: 'https://github.com/LegendaryJay/JavascriptFinal',
+        target: '_blank',
+      },
 
+      // https://balsamiq.cloud/si4kiz6/p7vg6g1
+    ]
+  ),
+  new Expando(
+    'Olympic Medals',
+    'React, Axios, C#, Entity Framework',
+    'This was an app was a tiny project that would help a single user keep track of olympic medals. The back-end and front-end were written by me. This is included to demonstrate my understanding of axios and react.',
+    null,
+    'info',
+    'commentary',
+    '',
+    [
+      {
+        title: 'Code',
+        icon: 'code',
+        href: 'https://github.com/LegendaryJay/Simple-react/tree/master',
+        target: '_blank',
+      },
+    ]
+  ),
   //
 
-  // new Expando(
-  //   'Wireframes',
-  //   'Balsamiq Wireframes, [Design]',
-  //   'I made these for my Web and Software capstone project'
-  // ),
   // new Expando(
   //   'Olympic Medals',
   //   'React, HTML/CSS/JS',
@@ -243,9 +406,7 @@ const projects = reactive([
         v-for="project in projects"
         :key="project.name"
       >
-        <div class="col">
-          <ExpandoCard :item="project"></ExpandoCard>
-        </div>
+        <ExpandoCard :item="project"></ExpandoCard>
       </div>
     </div>
   </div>
